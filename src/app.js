eda1e12dd44b11f36e12c5b03e8a7ab8744a1777
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2023-10-14 15:56:54 +0530

    fix: app crash if `NODE_ENV` env variable is not found

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // Cache environment variables
 require("dotenv").config();
 
-console.log("[app]", "Running in", process.env.NODE_ENV.toUpperCase(), "environment");
+console.log("[app]", "Running in", process.env.NODE_ENV?.toUpperCase(), "environment");
 
 // Connect to MongoDB
 require("./scripts/mongodb.script");
