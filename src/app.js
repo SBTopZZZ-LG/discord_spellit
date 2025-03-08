069ce5cd8f94ddf0fb8846b9e7a862dd8c789668
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2025-03-09 01:15:09 +0530

    refactor: remove unneeded logging

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 // Cache environment variables
 require("dotenv").config();
 
-console.log("[app]", "Running in", process.env.NODE_ENV?.toUpperCase(), "environment");
-
 // Connect to MongoDB
 require("./scripts/mongodb.script");
 
