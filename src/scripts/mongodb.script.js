7383f6b637b4148bd32e9f6ecb3dd4561ef83535
SBTopZZZ-LG <sbtopinkatti@gmail.com>
2025-03-08 22:25:16 +0530

    refactor: log out env variables for debugging

--- src/scripts/mongodb.script.js
+++ src/scripts/mongodb.script.js
@@ -4,10 +4,21 @@ const mongoose = require("mongoose");
 // Constants
 const { MONGODB_URI } = process.env;
 
+console.log(process.env);
+console.log(process.env.DISCORD_BOT_TOKEN);
+console.log(process.env.DISCORD_CLIENT_SECRET);
+console.log(process.env.MONGODB_URI);
+
 // Middlewares
 
-mongoose.connection.on("error", error => console.error("[mongodb.script]", error));
-mongoose.connection.on("connected", () => console.log("[mongodb.script]", "Connected to MongoDB"));
-mongoose.connection.on("disconnected", () => console.log("[mongodb.script]", "Disconnected from MongoDB"));
+mongoose.connection.on("error", (error) =>
+	console.error("[mongodb.script]", error)
+);
+mongoose.connection.on("connected", () =>
+	console.log("[mongodb.script]", "Connected to MongoDB")
+);
+mongoose.connection.on("disconnected", () =>
+	console.log("[mongodb.script]", "Disconnected from MongoDB")
+);
 
 mongoose.connect(MONGODB_URI);
